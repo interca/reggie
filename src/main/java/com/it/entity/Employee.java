593a834b28b13a9921a04f812cf967bfc64dfea9
hongsheng <1634340724@qq.com>
2022-09-14 04:13:07 +0800

    员工部分

---
+++ src/main/java/com/it/entity/Employee.java
@@ -0,0 +1,48 @@
+package com.it.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 员工实体类
+ * @author hyj
+ * @since 2022-9-14
+ */
+@Data
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String password;
+
+    private String phone;
+
+    private String sex;
+
+    /**
+     * 身份证
+     */
+    private String idNumber;
+
+    private Integer status;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+
+}
