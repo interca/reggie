cd79f8150745825ee6fd08c07888add7990f2d56
hongsheng <1634340724@qq.com>
2022-11-10 13:04:58 +0800

    结算功能

---
+++ src/main/java/com/it/service/impl/OrderDetailServiceImpl.java
@@ -0,0 +1,8 @@
+package com.it.service.impl;
+
+import com.it.service.OrderDetailService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderDetailServiceImpl implements OrderDetailService {
+}
---
+++ src/main/java/com/it/service/impl/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.it.service.impl;
+
+import com.it.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+}
